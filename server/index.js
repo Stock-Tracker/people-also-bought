3351f6687801a633729cf900cc118b51ab6eec73
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2020-01-01 01:52:31 -0500

    Adds people-also-bought route

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const PORT = process.env.PORT || 4550;
 const app = express();
+const controller = require('../controller/index.js');
 
 app.use(cors());
 app.use(express.static('client/dist'));
 app.use(express.static('client/public'));
+
+app.get('/people-also-bought/:ticker', controller.getPab)
 
 app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
