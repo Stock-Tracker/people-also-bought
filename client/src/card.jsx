085bdc009f8064e95962d8e4badd6e35d9db2047
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2020-01-08 10:04:26 -0500

    Adds price tag svg

--- client/src/card.jsx
+++ client/src/card.jsx
@@ -3,6 +3,13 @@ import React from 'react';
 const Card = ({ onMouseEnterOrLeave, index, name, price, percentChange, theme }) => {
   percentChange = percentChange > 0 ? `+${percentChange * 100}%` : `${percentChange * 100}%`;
 
+  // TODO: review how this works ...
+  const priceTag = <svg width="20" height="20" viewBox="0 0 20 20">
+    <g fillRule="evenodd" transform="translate(-4 -4)">
+      <path id="tag-a" d="M20.99975,8 C20.44775,8 19.99975,7.552 19.99975,7 C19.99975,6.448 20.44775,6 20.99975,6 C21.55175,6 21.99975,6.448 21.99975,7 C21.99975,7.552 21.55175,8 20.99975,8 M21.99975,4 L14.82775,4 C14.29775,4 13.78875,4.21 13.41375,4.585 L4.58575,13.414 C3.80475,14.195 3.80475,15.461 4.58575,16.242 L11.75675,23.414 C12.53775,24.195 13.80475,24.195 14.58575,23.414 L23.41375,14.586 C23.78875,14.211 23.99975,13.702 23.99975,13.172 L23.99975,6 C23.99975,4.896 23.10375,4 21.99975,4"></path>
+    </g>
+  </svg>
+
   return (
     <div className="pab-card" data-theme={theme} data-index={index}>
       <div className="pab-card-top">
@@ -12,7 +19,7 @@ const Card = ({ onMouseEnterOrLeave, index, name, price, percentChange, theme })
           onMouseEnter={onMouseEnterOrLeave}
           onMouseLeave={onMouseEnterOrLeave}
         >
-          <span>g</span>
+          <span className="price-tag">{priceTag}</span>
           <span className="pab-rating">88% Buy</span>
         </div>
       </div>
