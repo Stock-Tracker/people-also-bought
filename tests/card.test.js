6fc14f531af3ad60e857c848e7531cf21bc6d371
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2020-01-07 20:01:09 -0500

    Adds test for display of percentChange

--- tests/card.test.js
+++ tests/card.test.js
@@ -3,8 +3,28 @@ import { shallow } from 'enzyme';
 import Card from '../client/src/card.jsx'
 
 describe('Card', () => {
-  const wrapper = shallow(<Card></Card>);
   it('Contains a <div class="pab-card">', () => {
+    const wrapper = shallow(<Card></Card>);
     expect(wrapper.find('.pab-card')).toHaveLength(1);
+  });
+
+  it('percentChange displays a string', () => {
+    const percents = [
+      0,
+      0.0361,
+      -0.1602,
+      -0.752,
+      0.3,
+      0,
+      -0.006,
+      0.0054,
+      -0.0405
+    ];
+
+    percents.forEach(percent => {
+      const wrapper = shallow(<Card percentChange={percent}></Card>);
+      const string = wrapper.find('.pab-percent-change').text();
+      expect(string).toEqual(expect.any(String));
+    });
   });
 });
